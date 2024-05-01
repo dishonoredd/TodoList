33917932012161841f527dc2fd2eabfa4d2dfcbe
dishonorredd <suiratsasha06@gmail.com>
2024-05-01 21:22:24 +0300

    уауа

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,11 @@ function App() {
     setValue(todoText);
   });
 
+  function deleteTodo(i: number) {
+    result.splice(i, 1);
+    setTodoList(result);
+  }
+
   return (
     <div className="wrapper">
       <header className="header">
@@ -32,51 +37,46 @@ function App() {
         <button
           className="input__btn"
           onClick={() => {
-            if (todoText.length > 0 && todoText.length <= 100) {
+            if (todoText.length > 0) {
               setValue(todoText);
               result.unshift({ text: value });
-              console.log(value, todoText);
               setTodoList(result);
               setTodoText("");
             } else {
-              alert("Число введенных символов должно быть от 1 до 100");
+              alert(
+                "Число введенных символов изначально должно быть больше чем 0"
+              );
             }
           }}
         >
           Добавить
         </button>
       </div>
-      <div className="bottom">
-        <ul className="left">
-          {todoList.map((todo, i) => (
-            <li key={i}>
-              <Todo
-                changetext={changeText}
-                openButton={() => {
-                  setValue((todo.text = changeText));
-                  setChangeText("");
-                }}
-                setChangeText={(e) => {
-                  setChangeText(e.target.value);
-                }}
-                text={todo.text}
-                onClick={() => {
-                  result.splice(i, 1);
-                  setTodoList(result);
-                }}
-              />
-              <button
-                onClick={() => {
-                  result.splice(i, 1);
-                  setTodoList(result);
-                }}
-              >
-                Удалить
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+
+      <ul className="ultodo">
+        {todoList.map((todo, i) => (
+          <li key={i} className="element">
+            <Todo
+              changetext={changeText}
+              openButton={() => {
+                setValue((todo.text = changeText));
+                setChangeText("");
+              }}
+              setChangeText={(e) => {
+                setChangeText(e.target.value);
+              }}
+              text={todo.text}
+              onClick={() => {
+                result.splice(i, 1);
+                setTodoList(result);
+              }}
+            />
+            <button className="close" onClick={() => deleteTodo(i)}>
+              ✕
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -93,27 +93,34 @@ function Todo(props: TodoProps) {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div className="todo">{props.text}</div>
-      <button
-        onClick={() => {
-          setOpen(!open);
-          console.log(open);
-        }}
-      >
-        {(!open && "Изменить") || "Закрыть"}
-      </button>
-      {open && (
-        <div>
-          <input
-            type="text"
-            placeholder="Изменить"
-            onChange={props.setChangeText}
-          />
-          <button onClick={props.openButton}>✓</button>
-        </div>
-      )}
-    </>
+    <section className="todo__container">
+      <div className="todo">
+        <p className="todotext">{props.text}</p>
+      </div>
+      <div className="input__container">
+        <button
+          className="close__btn"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {(!open && "Изменить") || "Закрыть"}
+        </button>
+        {open && (
+          <div className="cont">
+            <input
+              className="changeinput"
+              type="text"
+              placeholder="Изменить"
+              onChange={props.setChangeText}
+            />
+            <button className="done__btn" onClick={props.openButton}>
+              ∨
+            </button>
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
 
