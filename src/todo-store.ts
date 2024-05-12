2a08f79bab08e3be20c56bd6cd2e9fde63b83331
dishonorredd <suiratsasha06@gmail.com>
2024-05-12 14:12:24 +0300

    white-space

--- src/todo-store.ts
+++ src/todo-store.ts
@@ -1,42 +1,42 @@
-import { autorun, makeAutoObservable } from "mobx"
+import { autorun, makeAutoObservable } from "mobx";
 
 export type Todo = {
-    text: string
-    id: string
-    completed: boolean
-}
+  text: string;
+  id: string;
+  completed: boolean;
+};
 
 class TodoStore {
-    todos: Todo[] = JSON.parse(localStorage.getItem("todoList") ?? "[]")
+  todos: Todo[] = JSON.parse(localStorage.getItem("todoList") ?? "[]");
 
-    constructor() {
-        makeAutoObservable(this)
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    get completedTodos() {
-        return this.todos.filter((t) => t.completed)
-    }
+  get completedTodos() {
+    return this.todos.filter((t) => t.completed);
+  }
 
-    get uncompletedTodos() {
-        return this.todos.filter((t) => !t.completed)
-    }
+  get uncompletedTodos() {
+    return this.todos.filter((t) => !t.completed);
+  }
 
-    addTodo = (todo: Todo) => {
-        this.todos.push(todo)
-    }
+  addTodo = (todo: Todo) => {
+    this.todos.unshift(todo);
+  };
 
-    completeTodo = (id: string) => {
-        const todo = this.todos.find((t) => t.id === id)
-        if (todo) todo.completed = true
-    }
+  completeTodo = (id: string) => {
+    const todo = this.todos.find((t) => t.id === id);
+    if (todo) todo.completed = true;
+  };
 
-    deleteTodo = (id: string) => {
-        this.todos = this.todos.filter((t) => t.id !== id)
-    }
+  deleteTodo = (id: string) => {
+    this.todos = this.todos.filter((t) => t.id !== id);
+  };
 }
 
-export const todoStore = new TodoStore()
+export const todoStore = new TodoStore();
 
 autorun(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoStore.todos))
-})
+  localStorage.setItem("todoList", JSON.stringify(todoStore.todos));
+});
